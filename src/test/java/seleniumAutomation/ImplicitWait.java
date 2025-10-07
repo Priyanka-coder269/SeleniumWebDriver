6572649c54690a4aa5bde44935f1cd334023b2dc
Priyanka <priyamalathika@gmail.com>
2025-10-07 23:08:30 +0530

    Selenium Waits

---
+++ src/test/java/seleniumAutomation/ImplicitWait.java
@@ -0,0 +1,24 @@
+package seleniumAutomation;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ImplicitWait {
+
+	public static void main(String[] args) {
+		WebDriver driver= new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+		
+		WebElement username =driver.findElement(By.xpath("//input[@name='username']"));
+		username.sendKeys("Admin");
+		driver.close();
+
+	}
+
+}

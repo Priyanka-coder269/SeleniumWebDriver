2df8f15eb455df9ae68f390a11e332de145499aa
Priyanka <priyamalathika@gmail.com>
2025-10-08 17:04:43 +0530

    WindowsHandling

---
+++ src/test/java/seleniumAutomation/WindowsHandling.java
@@ -0,0 +1,34 @@
+package seleniumAutomation;
+
+import java.time.Duration;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowsHandling {
+
+	public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	driver.manage().window().maximize();
+	
+	driver.findElement(By.xpath("//a[normalize-space()='OrangeHRM, Inc']")).click();
+	
+	Set<String> windowIDs=driver.getWindowHandles();
+	
+	for(String ID:windowIDs) {
+		String title=driver.switchTo().window(ID).getTitle();
+		
+		if(title.equals("Human Resources Management Software | HRMS | OrangeHRM") || title.equals("OrangeHRM")) {
+			driver.close();
+		}
+	}
+	
+	
+
+	}
+
+}
